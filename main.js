f935bbbce1576852b95ae79eb6e451415a66f3ac
Yuriy Golota <ypgolota@pm.me>
2022-05-28 12:09:31 -0400

    got the calc to work! phase 1

--- main.js
+++ main.js
@@ -1,7 +1,32 @@
-let buttons = document.querySelectorAll(".button");
+document.getElementById('calculator').addEventListener('click', (event) => {
+  const display = document.getElementById('display');
+  const input = event.target.textContent;
 
-for (let button of buttons) {
-  button.addEventListener("click", function (event) {
-    console.log(event.target.textContent);
-  });
-}
+  switch (input) {
+    case '0':
+    case '1':
+    case '2':
+    case '3':
+    case '4':
+    case '5':
+    case '6':
+    case '7':
+    case '8':
+    case '9':
+    case '.':
+    case '+':
+    case '-':
+    case '/':
+      display.textContent += input;
+      break;
+    case 'x':
+      display.textContent += '*';
+      break;
+    case 'C':
+      display.textContent = '';
+      break;
+    case '=':
+      display.textContent = eval(display.textContent);
+      break;
+  }
+});
